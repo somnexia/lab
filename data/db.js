caa79126c1094cdaec55e9885c9daeec65334e8b
Snus292 <georgiigiblov@gmail.com>
2024-11-04 23:02:32 +0200

    создан файл конфигурции бд для лендинг страницы

--- data/db.js
+++ data/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: '127.0.0.1',  // адрес вашего сервера базы данных
     user: 'root',       // имя пользователя базы данных
-    password: '', // пароль пользователя базы данных
+    password: null, // пароль пользователя базы данных
     database: 'courses' // имя базы данных
 });
 
