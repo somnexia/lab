816d3b8403855da83d7d9ce7445111640212cc27
Snus292 <georgiigiblov@gmail.com>
2024-11-07 20:13:33 +0200

    настроен главный серверный файл для маштабирования проекта и разделения структуры

--- app.js
+++ app.js
@@ -1,28 +1,48 @@
+const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes'); // Импортируем основной файл маршрутов
 const path = require("path")
 const exphbs = require("express-handlebars")
 const db = require("./data/db")
-
+const morgan = require('morgan');
 
 const app = express();
+
+app.use(morgan('dev')); // Это будет логировать запросы в консоль
+app.use((req, res, next) => {
+  try {
+    decodeURIComponent(req.path);
+    next();
+  } catch (error) {
+    res.status(400).send('Invalid URI');
+  }
+});
+
 const hbs = exphbs.create({
   defaultLayout: "page",
-  extname:"hbs"
+  extname: "hbs"
 })
+app.use(morgan('dev'));
+app.use(cors());
 app.use(bodyParser.json());
-app.engine("hbs",hbs.engine)
+app.use(express.json());
+app.engine("hbs", hbs.engine)
 app.set("view engine", "hbs")
 app.set("views", "views")
 
-app.use(express.static(path.join(__dirname,"public")))
-app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, "public")))
+app.use(express.urlencoded({ extended: true }))
 
 // Используем основной файл маршрутов
 app.use('/', routes);
 
+
+
+// Запуск сервера с обработкой ошибок
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+}).on('error', (error) => {
+  console.error(`Failed to start server on port ${port}:`, error);
 });
