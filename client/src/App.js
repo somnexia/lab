02e147487e1aa0ee86e1fa331547f5c028bcfedd
Snus292 <georgiigiblov@gmail.com>
2025-02-11 23:16:03 +0200

    Добавлены страницы для аутентификации (вход, регистрация, выход) и улучшена структура маршрутов; реализована логика отображения общих компонентов на основе текущего пути.

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import React from 'react';
 import ChemElementList from './components/ChemElementList';
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import Header from './partials/Header';
 import Footer from './partials/Footer';
 import Aside from './partials/Aside';
@@ -15,6 +15,11 @@ import StorageUnitList from "./components/StorageUnitС";
 import Inventory from "./pages/Inventory";
 import StorageUnit from "./components/StorageUnit";
 import CartOffcanvas from "./components/CartOffcanvas";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import SignOut from "./components/SignOut";
+import { ThemeProvider } from "./context/ThemeContext";
+
 
 
 
@@ -25,21 +30,46 @@ import CartOffcanvas from "./components/CartOffcanvas";
 
 function App() {
     return (
-
-        <Router>
-            <div className="page-wrapper">
-                <Header />
-                <Aside />
-                <Settings />
-                <Account />
-                <CartOffcanvas />
-                <Main />
-
-            </div>
-        </Router>
-
-
+        <ThemeProvider>
+            <Router>
+                <PageWrapper />
+            </Router>
+        </ThemeProvider>
     );
 }
+
+const PageWrapper = () => {
+    const location = useLocation();
+
+    // Список путей, где не отображаются общие компоненты
+    const noCommonComponentsPaths = ["/management/signin", "/management/signup", "/management/signout"];
+    const isSpecialPage = noCommonComponentsPaths.includes(location.pathname);
+
+    return (
+        <div className="page-wrapper">
+            {/* Показываем общие компоненты, если это не специальная страница */}
+            {!isSpecialPage && (
+                <>
+                    <Header />
+                    <Aside />
+                    <Settings />
+                    <Account />
+                    <CartOffcanvas />
+                </>
+            )}
+
+            {/* Основной контент */}
+            <Routes>
+                {/* Специальные страницы */}
+                <Route path="/management/signin" element={<SignIn />} />
+                <Route path="/management/signup" element={<SignUp />} />
+                <Route path="/management/signout" element={<SignOut />} /> 
+
+                {/* Все остальные страницы */}
+                <Route path="*" element={<Main />} />
+            </Routes>
+        </div>
+    );
+};
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -4,20 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { CartProvider } from "./context/CartContext";
+import { AuthProvider } from './context/AuthContext';
+
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <AuthProvider>
     <CartProvider>
       <App />
     </CartProvider>
-  </React.StrictMode>
+  </AuthProvider>
+
 
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
