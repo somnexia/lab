d4e57c0d80831ed7c7e0780673152036058caa11
Snus292 <georgiigiblov@gmail.com>
2025-03-01 17:09:36 +0200

    Добавлен компонент ResearchOverview для отображения общего обзора исследований.

---
+++ client/src/components/ResearchOverview.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class ResearchOverview extends Component {
+    state = {  } 
+    render() { 
+        return (
+            
+            <div></div>
+        );
+    }
+}
+ 
+export default ResearchOverview;
