2ce07ca5b3c3c985111fb8f8857dfff71f49806e
Snus292 <georgiigiblov@gmail.com>
2025-03-10 16:08:51 +0200

    Добавлено стилизация для элементов пагинации и создан компонент ParticipantDropdown

---
+++ client/src/components/ParticipantDropdown.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class ParticipantDropdown extends Component {
+    state = {}
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ParticipantDropdown;
