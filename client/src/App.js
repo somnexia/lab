6326d6fb42180e65304c1c97a2809b41dc5606ec
Snus292 <georgiigiblov@gmail.com>
2025-05-20 15:25:18 +0300

    Исправлены ошибки форматирования в файле App.js: добавлена точка с запятой в импорте App.scss

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 // import logo from './logo.svg'
-import "./App.scss"
+import "./App.scss";
 import './App.css';
 import React from 'react';
 import ChemElementList from './components/ChemElementList';
