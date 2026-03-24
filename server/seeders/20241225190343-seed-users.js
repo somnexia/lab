ddc46592dcc7c843414717d9881d7dba0184aa7d
somnexia <georgii.giblov@gmail.com>
2026-03-24 14:08:25 +0200

    feat: update user seeder to use hashed passwords and add new users

--- server/seeders/20241225190343-seed-users.js
+++ server/seeders/20241225190343-seed-users.js
@@ -1,16 +1,18 @@
 'use strict';
 
-const { ids } = require('webpack');
+const bcrypt = require('bcrypt');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
+    const password1 = await bcrypt.hash('1234', 10);
+    const password2 = await bcrypt.hash('1234', 10);
     await queryInterface.bulkInsert('users', [
       {
         id: 1,
         name: 'John Doe',
         email: 'john.doe@example.com',
-        password: '1234', // Замените на хэшированный пароль
+        password: password1, // Замените на хэшированный пароль
         employee_id: null,
         createdAt: new Date(),
         updatedAt: new Date()
@@ -19,15 +21,34 @@ module.exports = {
         id: 2,
         name: 'Jane Smith',
         email: 'jane.smith@example.com',
-        password: '1234', // Замените на хэшированный пароль
+        password: password2, // Замените на хэшированный пароль
         employee_id: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
+      {
+        id: 3,
+        name: 'ggg',
+        email: 'georgiigiblov@gmail.com',
+        password: '$2b$10$lBXn0WnFRo8neGX00e4Y1.B/VmkYzX0AH2ldnwW3OKP6tlUlBezGi', // уже захэширован
+        employee_id: 2,
+        createdAt: new Date('2025-03-17T12:38:45'),
+        updatedAt: new Date('2025-03-17T13:04:06')
+      },
+      {
+        id: 4,
+        name: 'fff',
+        email: 'giblov@gmail.com',
+        password: '$2b$10$yWtEJ2xVW.kQS2fGQEYVnedgAniNAeWVSwcx/NT1pbT7PiEa8JQYi', // уже захэширован
+        employee_id: 3,
+        createdAt: new Date('2025-03-17T12:41:20'),
+        updatedAt: new Date('2025-03-17T13:06:28')
+      }
     ]);
+    
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('users', null, {});
+    await queryInterface.bulkDelete('users', { id: [1, 2, 3, 4] }, {});
   },
 };
