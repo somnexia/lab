351860237b7e35a505a39c24d28d8c80b088c78d
somnexia <georgii.giblov@gmail.com>
2026-05-19 14:43:28 +0300

    chore: update license information, remove dev flags from package-lock, and set API_BASE to localhost

--- client/src/components/AdminLogsPage.js
+++ client/src/components/AdminLogsPage.js
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect, useMemo, useState } from "react";
 import axios from "axios";
 
-const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:3000";
+const API_BASE = "http://localhost:3000";
 
 const INITIAL_FILTERS = {
   search: "",
